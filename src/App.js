c83b88a9e7671b33aac5178afe469fc2245b0de1
Chathura <chathura.madura11@gmail.com>
2022-04-25 01:08:42 +0530

    make routing and add css to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+import Sidebar from './components/SideBar/Sidebar';
+import MainDash from './components/MainDash/MainDash';
+import RightSide from './components/RightSide/RightSide';
+import Customers from './components/Customers/customers';
+import Services from './components/Services/Services';
+import Products from './components/Products/Products';
 
 function App() {
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AppGlass">
+        
+        
+        <BrowserRouter>
+        <Sidebar />
+          <Routes>
+            <Route exact path="/" element={<MainDash />}></Route>
+            <Route path="/services" element={<Services/>}></Route>
+            <Route path="/customers" element={<Customers/>}> </Route>
+            <Route path="/products" element={<Products />}></Route>
+            <Route path="/analytics" element={<MainDash />}></Route>
+          </Routes>
+          <RightSide />
+        </BrowserRouter>
+
+        
+      </div>
     </div>
   );
 }
