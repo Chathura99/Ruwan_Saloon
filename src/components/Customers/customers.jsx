f6c426ddfa06cbda57de64bb34d76afac1948c6d
Chathura <chathura.madura11@gmail.com>
2022-04-25 01:11:53 +0530

    show clients

---
+++ src/components/Customers/customers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Customers.css"
+
+const Customers = () => {
+    return (
+        <div className="Customers">
+            <h1>Customers</h1>
+
+
+        </div>
+    );
+};
+
+export default Customers;
